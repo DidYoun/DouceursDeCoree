02f6a07a5158f7e76f761e8c15a665537f57b23d
Did Youn <didier.youn@gmail.com>
2017-02-11 19:12:04 +0100

    #master : add bands

--- app/Helpers/BandsHelper.php
+++ app/Helpers/BandsHelper.php
@@ -11,6 +11,7 @@
 namespace Helpers;
 
 use Models\Bands;
+use DateTime;
 
 class BandsHelper
 {
@@ -22,19 +23,87 @@ class BandsHelper
      *  Get Bands
      *          Get every bands or a band
      */
-    public function getBands($bandName, $all){
-        if($all)
-            return $this->band->getAllBands();
-        
+    public function getBands($bandName){
         return $this->band->getBand($bandName);
     }
 
+    /**
+     *  Create Band
+     *          Create a band based on the data entered
+     *  @param HTTP Request $request
+     *  @return Boolean
+     */
     public function createBand($request){
-        $params = array('name' => $request->getParsedBody()['name'],
-                        'length' => $request->getParsedBody()['membersLength'],
-                        'date' => $request->getParsedBody()['date'],
-                        'agency' => $request->getParsedBody()['agency']);
-                        
-        $this->band->createBands($params);
+        $upload = new UploadHelper();
+        $uploadPath = $upload->upload($request->getUploadedFiles());
+        $params = json_decode($request->getParams()['datas']);
+
+
+        return $this->band->createBand($params, $uploadPath);
+    }
+
+    /**
+     *  Get All Bands
+     *          Return every bands
+     *  @return array $band
+     */
+    public function getAllBands(){
+        return $this->band->getAllBands();
+    }
+
+    /**
+     *  Get Artist From Bands
+     *
+     */
+    public function getArtistFromBands($id){
+        return $this->band->getArtists($id);
+    }
+
+    /**
+     *  Remove Band
+     *      Remove a band based on it's id
+     *  @param Int id
+     */
+    public function removeBand($id){
+        // get the douceur of a group
+        return $this->band->removeBand($id);
+    }
+
+    /**
+     *  Update Band
+     *          Update a band based on it's id and the params
+     *  @param HTTP Request
+     *  @return Boolean
+     */
+    public function update($request){
+        $upload = new UploadHelper();
+        $uploadPath = $upload->upload($request->getUploadedFiles());
+        $data = json_decode($request->getParams()['datas']);
+
+        if($uploadPath)
+            $this->band->updateCover($uploadPath, $data->id);
+
+        if(count((array) $data) > 1)
+            $this->band->updateBand($data);
+
+        // update the douceur
+        return $this->band->updateDouceurBand($request);
+    }
+
+    /**
+     *  Get Date
+     *          Get a date based on a str date
+     *  @param String $strDate
+     *  @return date $date
+     */
+    public function getDate($strDate){
+        $date = new DateTime($strDate);
+
+        return date_format($date, "Y-m-d");
+    }
+
+    public function removeAllDouceur($request){
+        $ids = $request->getParsedBody()['delete'];
+        return $this->band->removeDouceur($ids);
     }
 }
--- public/assets/js/band.js
+++ public/assets/js/band.js
@@ -1,22 +1,67 @@
-const band = (function(){
+/**
+ *  Band Helper 
+ *          A method that control the band create view
+ *  @public
+ */
+const band = (function () {
     let listOfDouceur = {
-        group_name : 'foo',
-        list : []
+        group_name: 'foo',
+        list: []
     };
 
+    let douceursUpdate = Object.assign(Object.create({}), {
+        delete: [],
+        new: []
+    });
+
+    let newCoverValue = false;
+
+    // Create a reference to our Helper
     const helperFunc = Object.create(helper);
+
+    /**
+     *  Alert 
+     *          Show an alert to the user and close it ..
+     */
+    const alert = (err, succ) => {
+        let parent = document.getElementById('container');
+        if (err)
+            parent.insertAdjacentHTML('beforeend', `<div class="alert alert-danger" id='alert-douceur' role="alert">${err}</div>`);
+        else
+            parent.insertAdjacentHTML('beforeend', `<div class="alert alert-success" role="alert" id='alert-douceur'>Congratulations ${succ}</div>`);
+
+        // remove the alert after 2sec
+        setTimeout(() => {
+            document.getElementById('alert-douceur').remove();
+        }, 2000);
+    };
+
 
     /**
      *  Add Douceur
      *          add douceur to a group
      */
-    const addDouceur = function(){
+    const addDouceur = function (state = {}) {
+        console.log(this);
         let attr = this.getAttribute('data-id');
 
-        if(listOfDouceur.list.indexOf(attr) != -1)
+        if (listOfDouceur.list.indexOf(attr) != -1) {
+            alert('vous avez déja selectionner cette douceur');
             return;
+        }
+
 
         listOfDouceur.list.push(this.getAttribute('data-id'));
+
+        if (state.status === 'update') {
+            douceursUpdate.new.push(this.getAttribute('data-id'));
+            alert(`You've just add ${this.getAttribute('data-name')}`)
+        }
+
+        this.classList.remove('btn-info');
+        this.classList.add('btn-default');
+        this.nextElementSibling.classList.add('btn-info');
+        this.nextElementSibling.classList.remove('btn-default');
     };
 
     /**
@@ -24,40 +69,235 @@ const band = (function(){
      *          Remove a douceur from the list of doucer
      *  @param {string} 
      */
-    const removeDouceur = function(){
+    const removeDouceur = function () {
         let index = listOfDouceur.list.indexOf(this.getAttribute('data-id'));
-        listOfDouceur.list.splice(index, 1);
+
+        if (index !== -1) {
+            listOfDouceur.list.splice(index, 1);
+            //console.log(this.previousSibling);
+            this.previousElementSibling.classList.remove('btn-default');
+            this.previousElementSibling.classList.add('btn-info');
+            this.classList.remove('btn-info');
+            this.classList.add('btn-default');
+        }
     };
 
     /**
      *  Create Group Douceur
      *          Create a group of douceur based on the params..
      */
-    const createGroupDouceur = () => {
-        let name = document.getElementById('name').value;
-        let date = document.getElementById('date').value;
-        let agency = document.getElementById('agency').value;
+    const prepareGroupDouceur = () => {
+        let name = document.getElementById('name');
+        let date = document.getElementById('date');
+        let agency = document.getElementById('agency');
+        let imgFile = document.getElementById('cover-link').files[0];
+        let desc = document.getElementById('group');
 
-        listOfDouceur.name = name;
-        listOfDouceur.agency = agency;
-        listOfDouceur.date = date;
-        listOfDouceur.membersLength = listOfDouceur.list.length();
+        let fd = new FormData();
 
+        helperFunc.FilesHelper(imgFile)
+            .then(res => {
+                listOfDouceur.name = helperFunc.empty(name.value, 'name', name);
+                listOfDouceur.agency = helperFunc.empty(agency.value, 'agency', agency);
+                listOfDouceur.date = helperFunc.empty(date.value, 'date', date);
+                listOfDouceur.membersLength = listOfDouceur.list.length;
+                listOfDouceur.description = helperFunc.empty(desc.value, 'description', desc);
+                fd.append('datas', JSON.stringify(listOfDouceur));
+                fd.append('file', imgFile);
+            })
+            .then(createGroupDouceur.bind(null, fd))
+            .catch(err => {
+                alert(err);
+            });
+    };
+
+    /**
+     *  Create Group Douceur
+     *          Create a group of douceur
+     *  @param {Object} params
+     */
+    const createGroupDouceur = (params = {}) => {
         // make a request toward the back end
 
-        let req = new RequestBackend('/');
-    };
-    
+        let req = new RequestBackend('/band/new', 'POST', params, 'form-data');
+        req.prepare().execute()
+            .then(res => {
+                if (res.url)
+                    window.location.href = '/band';
+            })
+            .catch(err => {
+                return Promise.reject('err');
+            });
+    }
+
     /**
-     *  Init
-     *          Init the band page by adding the listener
-     *  @private
+     *  Delete Band 
+     *          Delete a band from the DB
      */
-    this.init = () => {
-        helperFunc.addListener('select', 'class', addDouceur);
-        helperFunc.addListener('unselect', 'class', removeDouceur);
-        helperFunc.addListener('create-group', 'id', createGroupDouceur, true);
+    const deleteBand = function () {
+        // first remove every member of the band 
+        removeAllBand().then(res => {
+                let id = this.getAttribute('data-id');
+                const req = new RequestBackend(`/band/${id}`, 'DELETE');
+                req.prepare().execute()
+                    .then(res => {
+                        console.log(res);
+                        if (res.url)
+                            window.location.href = res.url;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            })
+            .catch(err => {
+                alert(err)
+            });
+        // then remove the band 
+    }
+
+    /**
+     *  Remove Artist in Band 
+     *       remove an artist of a band    
+     */
+    const removeBand = function () {
+        let douceurLength = document.getElementsByClassName('douceur-rm').length;
+
+        if (douceurLength > 1) {
+            let band = document.getElementById(`douceur_${this.getAttribute('data-id')}`);
+            douceursUpdate.delete.push(`douceur_${this.getAttribute('data-id')}`);
+            band.remove();
+        } else {
+            // provided a feedback to the user 
+        }
     };
 
-    document.addEventListener('DOMContentLoaded', this.init);
+    const removeAllBand = () => {
+        let douceurs = document.getElementsByClassName('douceur-rm');
+        for (let douceur of douceurs) {
+            douceursUpdate.delete.push(douceur.id);
+        }
+
+
+
+        console.log(douceursUpdate);
+
+        return new Promise((resolve, reject) => {
+
+            if (douceursUpdate.delete.length === 0)
+                resolve(true);
+
+            const req = new RequestBackend('/band/removeAll', 'POST', douceursUpdate);
+
+            req.prepare().execute()
+                .then(res => {
+                    resolve(true);
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        });
+    };
+
+    /**
+     * Save State Date 
+     */
+    const saveStateDate = () => {
+        return currentDate = document.getElementById('date').value;
+    };
+
+    /**
+     * Update Band 
+     *      Update band 
+     * @private
+     */
+    const updateBand = function (currentDate) {
+        let fd = new FormData();
+        let data = {};
+        // get the id of the band 
+        let id = document.getElementById('container').getAttribute('band-id');
+
+        // retrieve the data from the input.. 
+        let inputData = document.getElementsByClassName('form-control');
+
+        for (let i of inputData) {
+            // If there's update data push threw the Object 
+            if (i.value && i.getAttribute('param') !== 'file')
+                if (i.getAttribute('param') !== 'date')
+                    data[i.getAttribute('param')] = i.value.trim();
+                else
+                    // check if the date has changed
+                    if (i.value !== currentDate)
+                        data[i.getAttribute('param')] = i.value.trim();
+
+            if (i.getAttribute('param') === 'file' && newCoverValue)
+                fd.append('file', newCoverValue);
+        }
+
+        // if there's no change we does not update the band
+        if (douceursUpdate.delete.length != 0 || douceursUpdate.new.length != 0)
+            data.band = douceursUpdate;
+
+
+        if (Object.keys(data).length === 0 && !newCoverValue)
+            return;
+        // add the id to the datas to send 
+        data.id = id;
+        fd.append('datas', JSON.stringify(data));
+        const req = new RequestBackend(`/band/update`, 'POST', fd, 'form-data');
+        req.prepare().execute()
+            .then(res => {
+                if (res.url)
+                    window.location.href = res.url;
+            })
+            .catch(err => {
+                alert(err);
+            });
+    }
+
+    const updateImg = function () {
+        helper.FilesHelper(this.files[0])
+            .then(res => {
+                document.getElementById('updateImg').src = res;
+                newCoverValue = this.files[0];
+            })
+            .catch(err => {
+                alert(err);
+            });
+    };
+
+    /**
+     *  Bootloader 
+     *          Init the band page by adding the listener depending of the page ID
+     *  @public
+     */
+    this.bootloader = () => {
+        let pageID = document.getElementById('container').getAttribute('page-id');
+
+        switch (pageID) {
+            case 'band':
+                //helperFunc.addListener('delete', 'class', deleteBand);
+                break;
+            case 'band-create':
+                helperFunc.addListener('select', 'class', addDouceur);
+                helperFunc.addListener('unselect', 'class', removeDouceur);
+                helperFunc.addListener('create-group', 'id', prepareGroupDouceur, true);
+                break;
+            case 'band-edit':
+                let date = saveStateDate();
+                helperFunc.addListener('removeGroup', 'id', deleteBand);
+                helperFunc.addListener('add', 'id', function () {
+                    $('#myModal').modal('toggle');
+                });
+                helperFunc.addListener('select', 'class', addDouceur, null, 'click', {
+                    status: 'update'
+                });
+                helper.addListener('delete', 'class', removeBand);
+                helperFunc.addListener('update', 'id', updateBand.bind(null, date));
+                helperFunc.addListener('file', 'id', updateImg, null, 'change');
+                break;
+        }
+    };
+
+
+    document.addEventListener('DOMContentLoaded', this.bootloader);
 }.bind({}))();
