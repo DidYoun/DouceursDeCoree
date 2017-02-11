198f2bab2017ec0d8eaf85e9a07667f8457c043c
Did Youn <didier.youn@gmail.com>
2017-02-11 19:04:49 +0100

    #jenkins : rollback

--- phpunit/TestSelenium.php
+++ phpunit/TestSelenium.php
@@ -29,7 +29,7 @@ class TestSelenium extends DriverHelper
     public function test404()
     {
         $this->url('/coucou');
-        $this->assertEquals("LOL", $this->title());
+        $this->assertEquals(self::DEFAULT_TITLE_NOT_FOUND, $this->title());
     }
 
     /**
