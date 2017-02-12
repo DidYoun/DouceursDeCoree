a76e0d69cf83fa906e044543570acb64b9d9eb76
Did Youn <didier.youn@gmail.com>
2017-02-12 16:45:51 +0100

    #phpunit : add phpunit doc for covering, finalize test on flash douceur and begin write test for image form submit

--- phpunit/Flash.php
+++ phpunit/Flash.php
@@ -16,6 +16,18 @@ class Flash
     public $config;
     /** @var PHPUnit_Extensions_Selenium2TestCase $selenium */
     protected $selenium;
+    /**
+     * @var string FLASH_STATE_SUCCESS
+     */
+    const FLASH_STATE_SUCCESS = "alert-success";
+    /**
+     * @var string FLASH_STATE_INFO
+     */
+    const FLASH_STATE_INFO = "alert-info";
+    /**
+     * @var string FLASH_STATE_ERROR
+     */
+    const FLASH_STATE_ERROR = "alert-error";
 
     /**
      * Flash constructor.
@@ -28,5 +40,36 @@ class Flash
         $this->config = AppPageObject::getSeleniumConfig();
     }
 
+    /**
+     * Check the flash state
+     *
+     * @param string $expectedState
+     * @return bool
+     */
+    public function getFlashMessage($expectedState)
+    {
+        try {
+            /** @var array $flash */
+            $flash = $this->selenium->byClassName('alert');
+            if (!isset($flash)) {
+                return false;
+            }
+            /** @var string $classList */
+            $classList = $flash->attribute('class');
+            /** @var array $classList */
+            $classList = explode(' ', $classList);
+            foreach ($classList as $className) {
+                if ($className !== $expectedState) {
+                    continue;
+                }
 
+                return true;
+            }
+            return false;
+        } catch (PHPUnit_Extensions_Selenium2TestCase_Exception $e) {
+            echo 'Cant retrieve the class list of the target item';
+        }
+
+        return false;
+    }
 }
