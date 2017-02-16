1b8f15bb3c0b037f296cf35a16c4e5e11fb05a72
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2017-02-16 01:56:01 +0100

    correcting image path for jenkins build

--- phpunit/config/selenium.php
+++ phpunit/config/selenium.php
@@ -27,20 +27,20 @@ return [
         'date'       => '12-02-2017',
         'agency'     => 'SM Entertainment',
         'description'=> '피에스타(FIESTAR)는 로엔엔터테인먼트 소속의 대한민국의 5인조 걸그룹이다. 2013년 9월 로엔엔터테인먼트의 레이블 분리로 콜라보따리 레이블 소속으로 활동 하였다.[1] 2015년 9월 22일 콜라보따리 레이블이 로엔트리 레이블로 편입되어 현재 로엔트리 레이블 소속으로 활동 중이다.[2]2012년부터 2014년까지 6인조였다가 2014년 3월 20일부로 멤버 체스카가 탈퇴해 5인조로 재편되었다.',
-        'media_path' => '/Users/lookitsmarc/Downloads/ji_ho_oh_my_girl_57296.jpg'
+        'media_path' => '/media/douceurscoree/hotesse.jpg'
     ],
     'SELENIUM_KEY_BAND_FORM_CREATE_INVALID' => [
         'name'       => 'Oh my girl',
         'date'       => 'kim jung un',
         'agency'     => 'Loen',
         'description'=> '오마이걸(OH MY GIRL)은 대한민국의 8인조 걸 그룹으로, 소속사는 WM 엔터테인먼트이다. 대중들의 마이 걸이 되고자 팀명을 로 지었다. 2015년 4월 20일 미니 1집 《OH MY GIRL》의 발매와 동시에 쇼케이스로 데뷔했다. 오마이걸 가 미국 음악전문매체 빌보드가 선정한 에 신인 걸그룹으로 유일하게 14위에 올랐다. [1] 2016년 8월 1일 오마이걸의 첫번째 리메이크 앨범 《내 얘길 들어봐》로 컴백하였다.',
-        'media_path' => '/Users/lookitsmarc/Downloads/ji_ho_oh_my_girl_57296.jpg'
+        'media_path' => '/media/douceurscoree/hotesse.jpg'
     ],
     'SELENIUM_KEY_BAND_FORM_EDIT' => [
         'name'       => 'Oh my girl',
         'date'       => '25-02-2017',
         'agency'     => 'Loen',
         'description'=> '오마이걸(OH MY GIRL)은 대한민국의 8인조 걸 그룹으로, 소속사는 WM 엔터테인먼트이다. 대중들의 마이 걸이 되고자 팀명을 로 지었다. 2015년 4월 20일 미니 1집 《OH MY GIRL》의 발매와 동시에 쇼케이스로 데뷔했다. 오마이걸 가 미국 음악전문매체 빌보드가 선정한 에 신인 걸그룹으로 유일하게 14위에 올랐다. [1] 2016년 8월 1일 오마이걸의 첫번째 리메이크 앨범 《내 얘길 들어봐》로 컴백하였다.',
-        'media_path' => 'media/douceurscoree/hotesse.jpg'
+        'media_path' => '/media/douceurscoree/hotesse.jpg'
     ]
 ];
